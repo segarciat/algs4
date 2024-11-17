0a68653a008d21475de68ec910f665ed7a149c3c
Sergio Garcia <segarciat314@gmail.com>
2024-11-17 14:44:18 -0500

    Complete Exercise 2.5.19

--- algs4sgt/app/src/main/java/com/segarciat/algs4/ch2/sec5/ex19/KendallTau.java
+++ algs4sgt/app/src/main/java/com/segarciat/algs4/ch2/sec5/ex19/KendallTau.java
@@ -1,5 +1,15 @@
 package com.segarciat.algs4.ch2.sec5.ex19;
 
+import com.segarciat.algs4.ch2.sec2.ex19.Inversions;
+
+import java.util.Arrays;
+
+/**
+ * <code>2.5.19)</code>
+ * Implements an algorithm to compute the Kendall tau distance between
+ * two given permutations.
+ * @author Sergio E. Garcia Tapia
+ */
 public final class KendallTau {
     private KendallTau() {}
 
@@ -8,11 +18,12 @@ public final class KendallTau {
      * both of which are the same length <code>n</code>, which are permutations
      * of the non-negative integers <code>0</code> through <code>n-1</code>,
      * with no duplicates. The method does not verify that the arrays are
-     * indeed permutations.
+     * indeed permutations, and it does not modify the arrays.
+     *
      * @param a A permutation of the non-negative integers.
      * @param b A permutation of the non-negative integers.
      * @return The number of pairs that are in different order in the two permutations.
-     * @throws NullPointerException if either array is <code>null</code>.
+     * @throws NullPointerException     if either array is <code>null</code>.
      * @throws IllegalArgumentException if the arrays are distinct length.
      */
     public static int distance(int[] a, int[] b) {
@@ -26,14 +37,93 @@ public final class KendallTau {
         if (n <= 1)
             return 0;
 
-        /**
-         * Ideas:
-         * - Compute the number of inversions (K-tau distance to identity) for both.
-         * Then do some operation on that.
-         * -
-         */
+        // Array b defines the sort values
+        int[] sortValue = new int[n];
+        for (int i = 0; i < n; i++)
+            sortValue[b[i]] = i;
 
+        // Neither array will be modified
+        int[] aCopy = new int[n];
+        for (int i = 0; i < n; i++)
+            aCopy[i] = a[i];
+        int[] aux = new int[n];
+        return count(aCopy, aux, sortValue, 0, n - 1);
+    }
 
-        return 0;
+    /**
+     * Helper recursive method for {@link KendallTau#distance(int[], int[])}, which operates
+     * similar to mergesort.
+     * @param a The array for which we are computing inversions.
+     * @param aux An auxiliary array for sorting.
+     * @param sortOrder An array defining the value of integers <code>0</code> through <code>n - 1</code>,
+     *                  where <code>n</code> is the length of <code>a</code> and <code>aux</code>.
+     * @param lo The lower index on which we are operating (inclusive).
+     * @param hi The upper index on which we are operating (inclusive).
+     * @return The number of inversions in <code>a</code> according to the order defined by <code>sortedOrder</code>.
+     */
+    private static int count(int[] a, int[] aux, int[] sortOrder, int lo, int hi) {
+        if (hi <= lo)
+            return 0;
+        int mid = lo + (hi - lo) / 2;
+        int outOfOrder = 0;
+        outOfOrder += count(a, aux, sortOrder, lo, mid);
+        outOfOrder += count(a, aux, sortOrder, mid + 1, hi);
+        outOfOrder += merge(a, aux, sortOrder, lo, mid, hi);
+        return outOfOrder;
+    }
+
+    /**
+     * Helper recursive method for {@link KendallTau#count(int[], int[], int[], int, int)},
+     * which merges sorted sub-arrays <code>a[lo..mid]</code> and <code>a[mid+1..hi]</code> into
+     * sorted array <code>a[lo..hi]</code> according to the order defined by <code>sortedOrder</code>.
+     * Th array <code>aux</code> is an auxiliary array of the same length.
+     * @return
+     */
+    private static int merge(int[] a, int[] aux, int[] sortOrder, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++)
+            aux[k] = a[k];
+
+        int i = lo;
+        int j = mid + 1;
+        int outOfOrder = 0;
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (sortOrder[aux[j]] < sortOrder[aux[i]]) {
+                outOfOrder += mid - i + 1;
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+        return outOfOrder;
+    }
+
+    public static void main(String[] args) {
+        int[] identity = {0, 1, 2, 3, 4, 5, 6};
+        int[] a = {0, 3, 1, 6, 2, 5, 4};
+        int[] b = {1, 0, 3, 6, 4, 2, 5};
+
+        // Kendall tau distance between a and b
+        System.out.printf("Kendall tau distance between a and b: %d%n", distance(a, b));
+        System.out.printf("Kendall tau distance between b and a: %d%n", distance(b, a));
+
+        // Inversions in a
+        System.out.println();
+        System.out.printf("Kendall tau distance between a and identity: %d%n", distance(a, identity));
+        System.out.printf("Kendall tau distance between identity and a: %d%n", distance(identity, a));
+        System.out.printf("Inversions in a: %d%n",
+                Inversions.inversions(Arrays.stream(a).boxed().toArray(Integer[]::new)));
+
+        // Inversions in b
+        System.out.println();
+        System.out.printf("Kendall tau distance between b and identity: %d%n", distance(b, identity));
+        System.out.printf("Kendall tau distance between identity and b: %d%n", distance(identity, b));
+        System.out.printf("Inversions in b: %d%n",
+                Inversions.inversions(Arrays.stream(a).boxed().toArray(Integer[]::new)));
+
     }
 }
--- algs4sgt/app/src/main/java/com/segarciat/algs4/ch2/sec5/ex19/README.md
+++ algs4sgt/app/src/main/java/com/segarciat/algs4/ch2/sec5/ex19/README.md
@@ -12,3 +12,58 @@ definitions are given:
 `0` and `n-1` appears exactly once.
 - The Kendall tau distance between two permutations is the number of pairs that are 
 in different order in the two rankings.
+
+My algorithm is a variation of my implementation of `com.segarciat.algs4.ch2.sec2.ex19.Inversions.inversions()`.
+Namely, it uses merge sort because it is linearithmic and it is stable. That method uses
+`less()`, which relies on the natural order of the elements being compared. Then, it uses
+merge sort to sort the array and detects inversions during the `merge()` calls.
+
+In this program, I chose one of the two arrays, permutation `b`, as defining an "order",
+and then sorted the other array, permutation `a`, according to the order defined by `b`.
+Once again, I employed merge-sort, and I detect the number of elements "out of order"
+(which are inversions relative to the order determined by `b`) in the `merge()` call.
+
+For example, given permutations `a` and `b`:
+
+```text
+a = [ 0, 3, 1, 6, 2, 5, 4 ]
+b = [ 1, 0, 3, 6, 4, 2, 5 ]
+```
+
+then we can think of `b` as the mapping:
+
+```text
+b[0] = 1
+b[1] = 0
+b[2] = 3
+b[3] = 6
+b[4] = 4
+b[5] = 2
+b[6] = 5
+```
+
+and we can use its inverse to define an order array `s`::
+
+```text
+s[1] = 0
+s[0] = 1
+s[3] = 2
+s[6] = 3
+s[4] = 4
+s[2] = 5
+s[5] = 6
+```
+
+so we have array
+
+```text
+s = [ 1, 0, 5, 2, 4, 6, 3 ]
+```
+
+Effectively, `s` defines an order.
+
+For example, say we want to see if `4` and `5` appear in different order.
+Note that `a[5] == 5` and `a[6] == 4`. The value of `5` according
+to `s` is `6`, because `s[5] == 6`, and the value of `4` according to `s` is `4`,
+because `s[4] == 4`. Since `s[a[5]] < s[a[4]]`, it follows that `5` and `4`
+are out of order  in `a` and `b`.
